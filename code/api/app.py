5931ba59fe6b4d131810458373b1778bb9f3fbae
André Dias <1140279@isep.ipp.pt>
2020-08-05 19:49:49 +0100

    Add sort definitions based on the matches with the sign db

--- code/api/app.py
+++ code/api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from webscraping.ws_priberam import Priberam
-from db.querydb import *
+from db.querydb import create_connection, check_pt_number_of_words
 
 app = Flask(__name__)
 CORS(app)
@@ -13,26 +13,56 @@ def index():
 
 @app.route('/search')
 def search():
+
+    #1. Get the word
     expression = request.args.get('query')
+
     if(expression != ""):
-        priberam = "https://dicionario.priberam.org/" + expression
-        infopedia = "https://www.infopedia.pt/dicionarios/lingua-portuguesa/" + expression
-        lexico = "https://www.lexico.pt/" + expression
-        wikipedia = "https://pt.wikipedia.org/wiki/" + expression
-        info = [ infopedia, lexico, wikipedia]
-        definition = Priberam.scrap(expression)
-        response_object = {'expression' : expression.capitalize(),
-            'definition' : definition,
-            'source' : priberam,
-            'additionalInfo' : info}
+
+        #2. Get the definitions of the word
+        definitions = Priberam.scrap(expression)
+
+        #3. Get the glosa of each definition
+        #axios.get(/convert?phrase=)
+
+        #4. Check the number of words from the definition that are in the DB
+        sortedMatchList = buildWordMatchList(definitions)
+
+        #5. Present the definition with the highest number of matches
+        response_object = buildResponse(expression, definition)
+
     else:
         response_object = None
     return jsonify(response_object)
 
-def testWord(word):
+def buildSortedMatchList(definitions):
+    matches = []
+    res = []
+    for i in len(definitions):
+        for j in len(definitions[i]):
+            numberOfWords = testSentence(definitions[i][j])
+            matches.append(numberOfWords)
+        res.append([x for _,x in sorted(zip(matches,definitions[i]))])
+        matches.clear()
+    return res
+
+def testSentence(sentence):
     database = "db/pythonsqlite.db"
     conn = create_connection(database)
-    row = select_pt_sign_by_word(conn, word)
+    res = check_pt_number_of_words(conn, sentence)
+    return res
+
+def buildResponse(expression, definition):
+    priberam = "https://dicionario.priberam.org/" + expression
+    infopedia = "https://www.infopedia.pt/dicionarios/lingua-portuguesa/" + expression
+    lexico = "https://www.lexico.pt/" + expression
+    wikipedia = "https://pt.wikipedia.org/wiki/" + expression
+    info = [ infopedia, lexico, wikipedia]
+    response = {'expression' : expression.capitalize(),
+                'definition' : definition,
+                'source' : priberam,
+                'additionalInfo' : info}
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
