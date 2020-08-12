ac3bcd52cb2a5ab9a83dd679ddde4c898508adfa
André Dias <1140279@isep.ipp.pt>
2020-08-12 23:24:15 +0100

    Fix small screen responsive dimensions

--- code/website/src/components/app/App.js
+++ code/website/src/components/app/App.js
@@ -34,9 +34,9 @@ class App extends Component {
                 <Layout className="layout">
                     <Header>
                         <Row justify="center">
-                            <Col xs={ 6 } sm={ 4 } md={ 3 } lg={ 3 } xl={ 2 }><div className="logo" /></Col>
-                            <Col xs={ 12 } sm={ 15 } md={ 3 } lg={ 16 } xl={ 18 }><h1> <b>E</b>xplicação <b>A</b>utomática de <b>C</b>onceitos </h1></Col>
-                            <Col xs={ 6 } sm={ 4 } md={ 3 } lg={ 3 } xl={ 2 }><Navbar /></Col>
+                            <Col xs={ 5.5 } sm={ 5 } md={ 4 } lg={ 3 } xl={ 2 }><div className="logo" /></Col>
+                            <Col xs={ 12 } sm={ 14 } md={ 15 } lg={ 16 } xl={ 18 }><h1> <b>E</b>xplicação <b>A</b>utomática de <b>C</b>onceitos </h1></Col>
+                            <Col xs={ 6 } sm={ 5 } md={ 4 } lg={ 3 } xl={ 2 }><Navbar /></Col>
                         </Row>
                     </Header>
                     <Content style={{ padding: '0 50px' }}>
@@ -45,8 +45,8 @@ class App extends Component {
                             <br />
                             <br />
                             <Row justify="center">
-                                <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 14 } xl={ 14 }><Display content={this.state.explanations}/></Col>
-                                <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 4 } xl={ 6 }><Avatar show={this.state.explanations}/></Col>
+                                <Col xs={ 12 } sm={ 12 } md={ 12 } lg={ 14 } xl={ 14 }><Display content={this.state.explanations}/></Col>
+                                <Col xs={ 12 } sm={ 12 } md={ 8 } lg={ 4 } xl={ 6 }><Avatar show={this.state.explanations}/></Col>
                             </Row>
                         </div>
                     </Content>
