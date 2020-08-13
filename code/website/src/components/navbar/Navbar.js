0e7c5c7f8b397e87e4affd55ddf62c7411d6416e
André Dias <1140279@isep.ipp.pt>
2020-08-13 01:04:59 +0100

    Fix navbar typo; Add flags to language menu

--- code/website/src/components/navbar/Navbar.js
+++ code/website/src/components/navbar/Navbar.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { Menu, Dropdown } from 'antd';
 import './Navbar.css';
 import { DownOutlined } from '@ant-design/icons';
+import FlagIcon from './FlagIcon.js';
 
 const menu = (
     <Menu>
     <Menu.Item>
     <a target="_blank" rel="noopener noreferrer">
-    Português
+    <FlagIcon code='pt' size='lg'/> Português
+    </a>
+    </Menu.Item>
+    <Menu.Item>
+    <a target="_blank" rel="noopener noreferrer">
+    <FlagIcon code='gb' size='lg'/> English
     </a>
     </Menu.Item>
     </Menu>
@@ -21,7 +27,7 @@ class Navbar extends Component {
             <div className="Navbar">
             <Dropdown overlay={menu}>
             <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-                    Linguagem <DownOutlined />
+                    Língua <DownOutlined />
             </a>
             </Dropdown>
             </div>
