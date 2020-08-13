f48b013a1b50826e7064fe2a3c365d2e33293836
André Dias <1140279@isep.ipp.pt>
2020-08-13 21:19:42 +0100

    Fix source and adicional information to be shown as default

--- code/website/src/components/display/Display.js
+++ code/website/src/components/display/Display.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Collapse, Popconfirm, message, Alert } from 'antd';
+import { Button, Card, Collapse, Popconfirm, message, Alert } from 'antd';
 import { CameraOutlined, DislikeOutlined, LikeOutlined } from '@ant-design/icons';
 import './Display.css';
 
@@ -26,7 +26,7 @@ class Display extends Component {
         let definition
         let feedback
         if(this.props.content != null) {
-            if (this.props.content.definition[0] == "Não foram encontrados resultados."){
+            if (this.props.content.definition[0] === "Não foram encontrados resultados."){
                 displayContent = <div><Alert
                     message="Erro!"
                     description="Não foram encontrados resultados."
@@ -37,7 +37,7 @@ class Display extends Component {
 
                 feedback =
                     <div id="feedback">
-                        <a href="#"><LikeOutlined /></a>
+                        <Button type="link"><LikeOutlined /></Button>
                         <Popconfirm
                             title="Deseja uma nova explicação?"
                             onConfirm={confirm}
@@ -45,7 +45,7 @@ class Display extends Component {
                             okText="Sim"
                             cancelText="Não"
                         >
-                            <a href="#"><DislikeOutlined /></a>
+                            <Button type="link"><DislikeOutlined /></Button>
                         </Popconfirm>
                     </div>
 
@@ -58,7 +58,7 @@ class Display extends Component {
                 definition = this.props.content.definition.map((item, index) => (
                     <div id={index}>
                         <Card title={`${index+1}. ${this.props.content.expression}`} style={{ width: '90%' }}>
-                            <p>{item[0]}  &nbsp; <a href={`https://www.google.com/search?q=${item[0]}&tbm=isch`} target="_blank"><CameraOutlined style={{ fontSize: '30 px'}} /></a></p>
+                            <p>{item[0]}  &nbsp; <a href={`https://www.google.com/search?q=${item[0]}&tbm=isch`} target="_blank" rel="noopener noreferrer"><CameraOutlined style={{ fontSize: '30 px'}} /></a></p>
                             <br />
                                 {feedback}
                         </Card>
@@ -70,7 +70,7 @@ class Display extends Component {
                 displayContent =
                     <div>
                             {definition}
-                        <Collapse accordion style={{ width: '90%' }}>
+                        <Collapse defaultActiveKey={['1','2']} style={{ width: '90%' }}>
                             <Panel header="Fontes" key="1">
                                 <p><a href={this.props.content.source}>{this.props.content.source}</a></p>
                             </Panel>
